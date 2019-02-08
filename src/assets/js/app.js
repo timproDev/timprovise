df46e8945aa52a5a77e5d21e0254ff8ac4eb9455
Schletter <tschlett@usnycmlh0egfd59.us.mrshmc.com>
2019-02-08 12:23:25 -0500

    more font changes and added a song

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,88 +8,37 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-import 'tablesaw/dist/tablesaw.jquery';
+// import 'tablesaw/dist/tablesaw.jquery';
 import libs from './lib/dependancies';
 window.libs = libs;
 
 $(document).foundation();
 
-// libs.AOS.init();
+function replaceContentInContainer(matchClass, content) {
+    var elems = document.getElementsByTagName('*'), i;
+    for (i in elems) {
+        if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
+                > -1) {
+            elems[i].innerHTML = content;
+        }
+    }
+}
 
-// SVG Injector
-// Elements to inject
-// var mySVGsToInject = document.querySelectorAll('img.inject-me');
-
-// Options
-// var injectorOptions = {
-//   evalScripts: 'once',
-//   pngFallback: 'assets/png'
-// };
-
-// var afterAllInjectionsFinishedCallback = function (totalSVGsInjected) {
-//   // Callback after all SVGs are injected
-//   console.log('We injected ' + totalSVGsInjected + ' SVG(s)!');
-// };
-
-// var perInjectionCallback = function (svg) {
-//   // Callback after each SVG is injected
-//   console.log('SVG injected: ' + svg);
-// };
-
-// create injector configured by options
-// var injector = new libs.svgInjector(injectorOptions);
-
-// Trigger the injection
-// injector.inject(
-//   mySVGsToInject,
-//   afterAllInjectionsFinishedCallback,
-//   perInjectionCallback
-// );
-
-// slick carousel
-// $(".content-carousel").slick({
-//   speed: 5000,
-// 	autoplay: true,
-// 	autoplaySpeed: 0,
-// 	cssEase: 'linear',
-//   slidesToShow: 5,
-// 	slidesToScroll: 1,
-//   infinite: true,
-//   swipeToSlide: true,
-// 	centerMode: true,
-//   focusOnSelect: true,
-//   // the magic
-//   responsive: [{
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         infinite: true
-//       }
-//     }, {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 2,
-//         dots: true
-//       }
-//     }, {
-//       breakpoint: 300,
-//       settings: "unslick" // destroys slick
-//     }]
-// });
-
-// tablesaw table plugin
-// $(function () {
-//   $(document)
-//     .foundation()
-//     .trigger('enhance.tablesaw');
-// });
-
-// var TablesawConfig = {
-//   swipeHorizontalThreshold: 15
-// };
-
-// app dashboard toggle
-// $('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
-//   e.preventDefault();
-//   $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
-// });
+(function greetingTime() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var greeting = "morning";
+  if (hours >= 12 && hours <= 16) {
+      greeting = "afternoon";
+      hours = hours - 12;
+  }
+  if (hours > 16 && hours <= 24) {
+      greeting = "evening";
+      hours = hours - 12;
+  }
+  if (hours == 0) {
+      hours = 12;
+      greeting = "afternoon";
+  }
+  replaceContentInContainer('js-update-greeting', greeting)
+})();
